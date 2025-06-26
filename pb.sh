10ed3d5431a410438dbf78298bea4a27509011a4
팰붕이 <107607815+palbungi@users.noreply.github.com>
2025-06-26 23:46:24 +0900

    Update pb.sh

--- pb.sh
+++ pb.sh
@@ -3,7 +3,7 @@ sudo timedatectl set-timezone Asia/Seoul
 
 # 도커&도커컴포즈 설치
 sudo groupadd docker
-sudo usermod -aG docker bykimott
+sudo usermod -aG docker $USER
 sudo apt update && sudo apt -y upgrade && sudo apt install -y nano && sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && yes | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.37.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
