37265287a98e706e6f2bd4c789c7551e9772e8a9
팰붕이 <107607815+palbungi@users.noreply.github.com>
2025-06-26 23:55:31 +0900

    Delete off.sh

--- off.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-HOST='bykimott'
-YAML_FILE="/home/$HOST/docker-compose.yml"
-CONTAINER_NAME="palworld"
-
-# 서버 재시작
-docker-compose -f "${YAML_FILE}" pull
-docker-compose -f "${YAML_FILE}" down
